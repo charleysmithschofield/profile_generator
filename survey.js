2162208db9adf4a15280b03dd6e674860dd33b42
charleysmithschofield <charleysmithschofield@gmail.com>
2024-02-19 19:30:47 -0700

    corrected some comments

--- survey.js
+++ survey.js
@@ -42,8 +42,6 @@ const rl = readline.createInterface({
 
 // Asking the first question to the user through the readline interface
 rl.question('What\'s your name? Nicknames are also acceptable :) ', (name) => {
-  // Logging the user's response along with a thank you message
-  console.log(`My name is, ${name}.`);
 
   // Asking the second question after the first one is answered
   rl.question('What\'s an activity you like doing? ', (activity) => {
@@ -60,13 +58,11 @@ rl.question('What\'s your name? Nicknames are also acceptable :) ', (name) => {
           // Asking the sixth question after the fifth is answered
           rl.question('Which sport is your absolute favourite? ', (sport) => {
 
-            // Asking the sixth question after the fifth is answered
+            // Asking the seventh question after the sixth is answered
             rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (superpower) => {
-
 
               // Logging the user's response
               console.log(`My name is, ${name}. I enjoy ${activity}, during this I like to listen to ${music}. My favourite meal is ${meal}, for ${meal} my favourite is ${favourite}. When I am not eating my favourite meal, I like to play ${sport}. My superpower is ${superpower}`);
-
 
               // Closing the readline interface after both questions are answered
               rl.close();
